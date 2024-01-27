a08e29cefea78414fd8738d037a2e1b455cc98bd
amit <amitnoa.av@gmail.com>
2024-01-27 17:19:35 +0200

    it should be fixed now

--- myshell.c
+++ myshell.c
@@ -6,7 +6,6 @@
 #include <linux/limits.h>
 #include <string.h>
 #include "LineParser.h"
-#include <fcntl.h>
 
 #define MAX_INPUT_SIZE 2048
 
@@ -82,66 +81,80 @@ cmdLine *parseInput(char *input)
 
 void execute(cmdLine *pCmdLine, int debugFlag)
 {
-    pid_t child_pid = fork();
-
-    if (child_pid == -1)
+    if (strcmp(pCmdLine->arguments[0], "cd") == 0)
     {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    if (child_pid == 0)
-    {
-        // Child process
-        if (debugFlag)
+        // Handle "cd" command separately
+        if (chdir(pCmdLine->arguments[1]) == -1)
         {
-            fprintf(stderr, "PID: %d\n", getpid());
-            fprintf(stderr, "Executing command: %s\n", pCmdLine->arguments[0]);
-        }
-
-        // Redirect input if needed
-        if (pCmdLine->inputRedirect != NULL)
-        {
-            int input_fd = open(pCmdLine->inputRedirect, O_RDONLY);
-            if (input_fd == -1)
-            {
-                perror("open inputRedirect");
-                _exit(EXIT_FAILURE);
-            }
-            dup2(input_fd, STDIN_FILENO);
-            close(input_fd);
-        }
-
-        // Redirect output if needed
-        if (pCmdLine->outputRedirect != NULL)
-        {
-            int output_fd = open(pCmdLine->outputRedirect, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-            if (output_fd == -1)
-            {
-                perror("open outputRedirect");
-                _exit(EXIT_FAILURE);
-            }
-            dup2(output_fd, STDOUT_FILENO);
-            close(output_fd);
-        }
-
-        // Execute the command
-        if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) == -1)
-        {
-            perror("execvp");
-            _exit(EXIT_FAILURE); // _exit is used to terminate the process if execvp fails
+            perror("chdir");
+            fprintf(stderr, "Error: Unable to change directory to %s\n", pCmdLine->arguments[1]);
         }
     }
     else
     {
-        // Parent process
-        int status;
-        waitpid(child_pid, &status, 0);
+        pid_t child_pid = fork();
 
-        if (status == -1)
+        if (child_pid == -1)
         {
-            perror("waitpid");
+            perror("fork");
             exit(EXIT_FAILURE);
+        }
+
+        if (child_pid == 0)
+        {
+            // Child process
+            if (debugFlag)
+            {
+                fprintf(stderr, "PID: %d\n", getpid());
+                fprintf(stderr, "Executing command: %s\n", pCmdLine->arguments[0]);
+            }
+
+            // Handle input redirection
+            if (pCmdLine->inputRedirect != NULL)
+            {
+                FILE *inputFile = fopen(pCmdLine->inputRedirect, "r");
+                if (inputFile == NULL)
+                {
+                    perror("fopen");
+                    _exit(EXIT_FAILURE);
+                }
+                dup2(fileno(inputFile), STDIN_FILENO);
+                fclose(inputFile);
+            }
+
+            // Handle output redirection
+            if (pCmdLine->outputRedirect != NULL)
+            {
+                FILE *outputFile = fopen(pCmdLine->outputRedirect, "w");
+                if (outputFile == NULL)
+                {
+                    perror("fopen");
+                    _exit(EXIT_FAILURE);
+                }
+                dup2(fileno(outputFile), STDOUT_FILENO);
+                fclose(outputFile);
+            }
+
+            if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) == -1)
+            {
+                perror("execvp");
+                _exit(EXIT_FAILURE); // Use _exit to terminate the process if execvp fails
+            }
+        }
+        else
+        {
+            // Parent process
+            if (pCmdLine->blocking)
+            {
+                int status;
+                waitpid(child_pid, &status, 0);
+
+                if (status == -1)
+                {
+                    perror("waitpid");
+                    exit(EXIT_FAILURE);
+                }
+            }
         }
     }
 }
