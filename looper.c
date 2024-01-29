c2d16fd0bc95d9e257e9a5a64ee5a93db2fac1b9
amit <amitnoa.av@gmail.com>
2024-01-29 10:17:11 +0200

    changed looper back to default

--- looper.c
+++ looper.c
@@ -4,47 +4,33 @@
 #include <signal.h>
 #include <string.h>
 
-void handler(int sig);
+void handler(int sig)
+{
+	printf("\nRecieved Signal : %s\n", strsignal(sig));
+	if (sig == SIGTSTP)
+	{
+		signal(SIGTSTP, SIG_DFL);
+	}
+	else if (sig == SIGCONT)
+	{
+		signal(SIGCONT, SIG_DFL);
+	}
+	signal(sig, SIG_DFL);
+	raise(sig);
+}
 
 int main(int argc, char **argv)
 {
-    printf("Starting the program\n");
 
-    // Set signal handlers
-    signal(SIGINT, handler);
-    signal(SIGTSTP, handler);
-    signal(SIGCONT, handler);
+	printf("Starting the program\n");
+	signal(SIGINT, handler);
+	signal(SIGTSTP, handler);
+	signal(SIGCONT, handler);
 
-    while (1)
-    {
-        sleep(1);
-    }
+	while (1)
+	{
+		sleep(1);
+	}
 
-    return 0;
-}
-
-/*
-task 0b
-*/
-void handler(int sig)
-{
-    // Print the received signal with a message for debbugging purposes
-    printf("\nReceived Signal: %s\n", strsignal(sig));
-
-    // Set the default signal handler to handle the signal
-    signal(sig, SIG_DFL);
-
-    // Raise the signal again so that the default signal handler can handle it
-    raise(sig);
-
-    // After handling SIGCONT, reinstate the custom handler for SIGTSTP
-    if (sig == SIGCONT)
-    {
-        signal(SIGTSTP, handler);
-    }
-    // After handling SIGTSTP, reinstate the custom handler for SIGCONT
-    else if (sig == SIGTSTP)
-    {
-        signal(SIGCONT, handler);
-    }
+	return 0;
 }
